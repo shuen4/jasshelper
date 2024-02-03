8db64b481f2321bdd136c9813830c0c7d18967de
sotzaii_shuen <55835583shuen@gmail.com>
2024-02-04 02:02:22 +0800

    update script

--- _move_file.bat
+++ _move_file.bat
@@ -5,6 +5,11 @@ move /y grimoire\clijasshelper.exe output
 move /y grimoire\grimoirecaller.exe output
 move /y grimoire\grimoirejasshelper.exe output
 move /y wehelper\jasshelperdll.dll output
-move /y wehelper\jasshelperdll.dll output
 move /y wehelper\jasshelperdll.rsm output
 move /y wehelper\jasshelperinstaller.exe output
+@REM create zip file
+cd output
+del output.zip
+del debug_symbol.zip
+7z a -mx9 -x!jasshelperdll.rsm output.zip *
+7z a -mx9 debug_symbol.zip jasshelperdll.rsm
