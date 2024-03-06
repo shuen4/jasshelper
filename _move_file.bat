9d2f2f84c97af003afb57b347881dba74295751b
sotzaii_shuen <55835583shuen@gmail.com>
2024-03-07 01:10:37 +0800

    update script and add "sfmpq.dll"

--- _move_file.bat
+++ _move_file.bat
@@ -2,14 +2,15 @@
 @REM move compiled file to output folder
 move /y cons\consolejasser.exe output
 move /y grimoire\clijasshelper.exe output
+move /y grimoire\grimoirejasshelper.exe output\jasshelper.exe
 move /y grimoire\grimoirecaller.exe output
-move /y grimoire\grimoirejasshelper.exe output
-move /y wehelper\jasshelperdll.dll output
-move /y wehelper\jasshelperdll.rsm output
+move /y wehelper\jasshelperdll.dll output\jasshelper.dll
+move /y wehelper\jasshelperdll.rsm output\jasshelper.rsm
 move /y wehelper\jasshelperinstaller.exe output
+copy /y jasshelper.cgt output
 @REM create zip file
 cd output
 del output.zip
 del debug_symbol.zip
-7z a -mx9 -x!jasshelperdll.rsm output.zip *
-7z a -mx9 debug_symbol.zip jasshelperdll.rsm
+7z a -mx9 -x!jasshelper.rsm output.zip *
+7z a -mx9 debug_symbol.zip jasshelper.rsm
